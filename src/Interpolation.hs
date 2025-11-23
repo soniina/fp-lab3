f8e3d0cbbb591271948388591e4b8631e4337ae9
soniina <sonikpav@gmail.com>
2025-11-23 20:59:32 +0300

    feat: add linearSpline func

--- src/Interpolation.hs
+++ src/Interpolation.hs
@@ -1,9 +1,21 @@
-module Interpolation (Point, linear, newton) where
+module Interpolation (Point, linearSpline, newton) where
 
 type Point = (Double, Double)
 
 linear :: Point -> Point -> Double -> Double
 linear (x0, y0) (x1, y1) x = y0 + (x - x0) * (y1 - y0) / (x1 - x0)
+
+linearSpline :: [Point] -> Double -> Double
+linearSpline points x =
+  let intervals = zip points (tail points)
+      match (_, (x2, _)) = x <= x2
+      validIntervals = dropWhile (not . match) intervals
+
+      (pLeft, pRight) =
+        if null validIntervals
+          then last intervals
+          else head validIntervals
+   in linear pLeft pRight x
 
 newton :: [Point] -> Double -> Double
 newton points x =
